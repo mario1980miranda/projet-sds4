cc186d06bd540bedbdfb61668420ae615605e377
Mario Luiz Miranda <mario_chan15@hotmail.com>
2021-09-11 13:48:47 -0400

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,24 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            Page accueil
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analysez vos performances de vente sous différents angles</p>
+                    <hr />
+                    <p>Cette application consiste à afficher un tableau de bord à partir des données fournies par un back-end construit avec Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Accès la table de bord
+                    </Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
