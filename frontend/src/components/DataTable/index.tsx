fa48f759d8194ec31e7fe5697dbce1c387123da1
Mario Luiz Miranda <mario_chan15@hotmail.com>
2021-09-12 21:04:26 -0400

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 const DataTable = () => {
+
+    const[activePage, setActivePage] = useState(0);
 
     const [page, setPage] = useState<SalePage>({
         first: true,
@@ -15,38 +18,44 @@ const DataTable = () => {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
             .then(response => {
                 setPage(response.data);
             })
-    }, []);
+    }, [activePage]);
+
+    const changePage = (index: number) => {
+        setActivePage(index);
+    }
 
     return (
-        <div className="table-responsive">
-            <table className="table table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Vendeur</th>
-                        <th>Clientes visités</th>
-                        <th>Accords conclus</th>
-                        <th>Valeur</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {page.content?.map(item => (
-                        <tr key={item.id}>
-                            <td>{formatLocalDate(item.date, "yyyy-MM-dd")}</td>
-                            <td>{item.vendeur.nom}</td>
-                            <td>{item.visite}</td>
-                            <td>{item.affairs}</td>
-                            <td>{item.montant.toFixed(2)}</td>
+        <>
+            <Pagination page={page} onPageChange={changePage}/>
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Vendeur</th>
+                            <th>Clientes visités</th>
+                            <th>Accords conclus</th>
+                            <th>Valeur</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-
+                    </thead>
+                    <tbody>
+                        {page.content?.map(item => (
+                            <tr key={item.id}>
+                                <td>{formatLocalDate(item.date, "yyyy-MM-dd")}</td>
+                                <td>{item.vendeur.nom}</td>
+                                <td>{item.visite}</td>
+                                <td>{item.affairs}</td>
+                                <td>{item.montant.toFixed(2)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
